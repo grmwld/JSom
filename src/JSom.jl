5a4700ea55a08bdb02f56b211fc05e1202ffe19c
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2015-10-22 10:55:49 +0200

    Keeps track of iterations and epochs as attributes

--- src/JSom.jl
+++ src/JSom.jl
@@ -25,6 +25,8 @@ type SOM
     neigx::Array{Int, 1}
     neigy::Array{Int, 1}
     T::Float64
+    t::Int
+    epochs::Int
     decay_function::Function
     dist_function::Function
 
@@ -33,6 +35,8 @@ type SOM
         weights = rand(x, y, input_len) * 2 - 1
         this = new()
         this.T = 0
+        this.t = 0
+        this.epochs = 0
         this.sigma = sigma
         this.learning_rate = learning_rate
         this.weights = broadcast(/, weights, mapslices(norm, weights, [1, 2]))
@@ -60,9 +64,10 @@ function neighborhood(som, c, σ)
 end
 
 
-function update(som::SOM, sample::Array, winner::Tuple, epoch::Int)
-    η = som.decay_function(som.learning_rate, epoch, som.T)
-    σ = som.decay_function(som.sigma, epoch, som.T)
+function update(som::SOM, sample::Array, winner::Tuple)
+    som.t += 1
+    η = som.decay_function(som.learning_rate, som.t, som.T)
+    σ = som.decay_function(som.sigma, som.t, som.T)
     g = neighborhood(som, winner, σ) * η
     sample = reshape(sample, (1, 1, length(sample)))
     for k in eachindex(g)
@@ -89,12 +94,12 @@ function activate(som::SOM, sample::Array)
 end
 
 
-function train_random(som::SOM, data::Array, num_epoch::Int)
-    init_T(som, num_epoch)
-    for epoch = 0:num_epoch
+function train_random(som::SOM, data::Array, num_iter::Int)
+    init_T(som, num_iter)
+    for t = 0:num_iter
         i = rand(1:size(data, 1))
         sample = data[i, :]
-        update(som, sample, winner(som, sample), epoch)
+        update(som, sample, winner(som, sample))
     end
 end
 
@@ -111,8 +116,8 @@ function quantization_error(som::SOM, data::Array)
 end
 
 
-function init_T(som::SOM, num_epoch::Int)
-    som.T = num_epoch / 2
+function init_T(som::SOM, num_iter::Int)
+    som.T = num_iter / 2
 end
 
 
